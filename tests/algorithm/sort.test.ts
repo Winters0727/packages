2800857ec40b551a7531421a54148b29688c9cd4
winters <winters0727@gmail.com>
2023-06-05 22:57:06 +0900

    feat: 병합정렬 테스트 예제 파일 생성

---
+++ tests/algorithm/sort.test.ts
@@ -0,0 +1,5 @@
+import { useMergeSort } from "../../utils/algorithm/sort";
+
+describe("병합정렬 테스트", () => {
+  const { divideValues, mergeValues, mergeSort } = useMergeSort();
+});
---
+++ utils/algorithm/sort.ts
@@ -0,0 +1,19 @@
+export const useMergeSort = () => {
+  const divideValues = (arr: number[]) => {
+    return [];
+  };
+
+  const mergeValues = (arr: number[]) => {
+    return [];
+  };
+
+  const mergeSort = (arr: number[]) => {
+    return [];
+  };
+
+  return {
+    divideValues,
+    mergeValues,
+    mergeSort,
+  };
+};
