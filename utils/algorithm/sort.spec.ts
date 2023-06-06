a57c101275260cbc39656f56bcf094fb0f709a3f
winters <winters0727@gmail.com>
2023-06-06 23:18:50 +0900

    feat: 병합정렬 코드 TDD 작성

--- utils/algorithm/sort.spec.ts
+++ utils/algorithm/sort.spec.ts
@@ -3,18 +3,22 @@ import { useMergeSort } from "./sort";
 describe("병합정렬", () => {
   const { divideValues, mergeValues, mergeSort } = useMergeSort();
 
-  test("값 나누기", () => {
-    expect(divideValues([])).toEqual([]);
-    expect(divideValues([4, 3, 2, 1])).toContainEqual([[1], [2], [3], [4]]);
+  test("divideValues 함수 테스트", () => {
+    expect(divideValues([])).toEqual([[], []]);
+    expect(divideValues([4, 3, 2, 1])).toEqual([
+      [4, 3],
+      [2, 1],
+    ]);
   });
 
-  test("값 합치기", () => {
+  test("mergeValues 함수 테스트", () => {
     expect(mergeValues([], [])).toEqual([]);
-    expect(mergeValues([3, 2], [4, 1])).toStrictEqual([1, 2, 3, 4]);
+    expect(mergeValues([3, 2], [4, 1])).toEqual([3, 2, 4, 1]);
   });
 
-  test("병합 및 정렬", () => {
+  test("mergeSort 함수 테스트", () => {
     expect(mergeSort([])).toEqual([]);
     expect(mergeSort([4, 3, 2, 1])).toStrictEqual([1, 2, 3, 4]);
+    expect(mergeSort([5, 6, 3, 4, 1, 2])).toStrictEqual([1, 2, 3, 4, 5, 6]);
   });
 });
--- utils/algorithm/sort.ts
+++ utils/algorithm/sort.ts
@@ -1,14 +1,49 @@
-export const useMergeSort = () => {
-  const divideValues = (arr: number[]) => {
-    return [];
+interface MergeSort {
+  mergeValues: (arr1: number[], arr2: number[]) => number[];
+  divideValues: (arr: number[]) => [number[], number[]];
+  mergeSort: (arr: number[]) => number[];
+}
+
+export const useMergeSort: () => MergeSort = () => {
+  const mergeValues = (arr1: number[], arr2: number[]) => {
+    const sortedArray: number[] = [];
+
+    const copiedArray1 = arr1.slice();
+    const copiedArray2 = arr2.slice();
+
+    while (copiedArray1.length > 0 && copiedArray2.length > 0) {
+      if (copiedArray1[0] <= copiedArray2[0]) {
+        const smallerValue = copiedArray1.shift();
+        smallerValue && sortedArray.push(smallerValue);
+      } else {
+        const smallerValue = copiedArray2.shift();
+        smallerValue && sortedArray.push(smallerValue);
+      }
+    }
+
+    return copiedArray1.length > 0
+      ? sortedArray.concat(copiedArray1)
+      : sortedArray.concat(copiedArray2);
   };
 
-  const mergeValues = (arr1: number[], arr2: number[]) => {
-    return [];
+  const divideValues = (arr: number[]) => {
+    const standardIndex = Math.floor(arr.length / 2);
+
+    const leftArray = arr.slice(0, standardIndex);
+    const rightArray = arr.slice(standardIndex, arr.length);
+
+    return [leftArray, rightArray] as [number[], number[]];
   };
 
   const mergeSort = (arr: number[]) => {
-    return [];
+    if (arr.length <= 1) return arr;
+
+    let [leftArray, rightArray] = divideValues(arr);
+
+    if (leftArray.length > 1) leftArray = mergeSort(leftArray);
+    if (rightArray.length > 1) rightArray = mergeSort(rightArray);
+
+    return mergeValues(leftArray, rightArray);
   };
 
   return {
